00ec742dfd6697f838479a22d7f502a39f6e84b2
VMLujanJr <vmlujanjr@outlook.com>
2022-06-12 01:45:01 -0500

    added some user routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,12 +1,85 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Comment } = require('../../models');
 
 // GET ALL /api/users
 router.get('/', (req, res) => {
     User.findAll({
         attributes: { exclude: ['password'] }
     })
-    .then(dbUserData => res.json(dbUserData));
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// GET ONE /api/users/:id
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'content', 'createdAt']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'createdAt'],
+                include: {
+                    model: Post,
+                    attributes: ['title']
+                }
+            }
+        ]
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user was found.'});
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// POST /api/users
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+    .then(dbUserData => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    });
+});
+
+// POST /api/users/login
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+
+        }
+        
+    })
+
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -8,12 +8,26 @@ const sequelize = require('./config/connection.js');
 /* [ Node.js path module provides utilities for working with file and directory paths ] */
 const path = require('path');
 
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+const sess = {
+    secret: 'secretomg',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public'))); // uses the 'public' folder as the root directory for static files
+app.use(session(sess));
 app.use(routes); // turn on routes
 
 sequelize.sync({ force: false }).then(() => {
